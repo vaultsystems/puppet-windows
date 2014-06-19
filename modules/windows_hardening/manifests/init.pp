9d1c4768d44fd02268f08424e1554430369efafb
noma4i <noma4i@gmail.com>
2014-06-19 18:40:19 +1000

    working version

--- modules/windows_hardening/manifests/init.pp
+++ modules/windows_hardening/manifests/init.pp
@@ -1,19 +1,30 @@
 define windows_hardening($os = $title, $ensure = 'present') {
   file {"C:\\Windows\\TEMP\\policy.inf":
     ensure  => file,
-    source_permissions => 'ignore',
     source  => 'puppet:///modules/windows_hardening/policy.inf'
+  } ->
+  file {"C:\\Windows\\TEMP\\gpo.reg":
+    ensure  => file,
+    source  => 'puppet:///modules/windows_hardening/gpo.reg'
   } ->
   exec { "Importing Policies":
     command => '
-      secedit /import /cfg "C:\\Windows\\TEMP\\policy.inf" /db "C:\\Windows\\TEMP\\policyDB.sdb"
+      if ((Test-Path "C:\Windows\Temp\policyDB.sdb") -eq $true){ rm "C:\Windows\Temp\policyDB.sdb" }
+      secedit /import /cfg "C:\Windows\Temp\policy.inf" /db "C:\Windows\Temp\policyDB.sdb"
     ',
     logoutput => true,
     provider  => powershell,
   } ->
   exec { "Applying configuration":
     command => '
-      secedit /configure /db "C:\\Windows\\TEMP\\policyDB.sdb"
+      secedit /configure /db "C:\Windows\Temp\policyDB.sdb"
+    ',
+    logoutput => true,
+    provider  => powershell,
+  } ->
+  exec { "Copy ADMX Templates":
+    command => '
+      iex "cmd.exe /c \'regedit.exe /s C:\Windows\Temp\gpo.reg\'"
     ',
     logoutput => true,
     provider  => powershell,
